9695ee7804cd7e49ddd0e3c86f1ebb1849a6f926
imranmauludi12 <thesolouser@gmail.com>
2022-02-05 18:40:46 +0700

    login and register_1

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,8 @@ const session = require("express-session");
 const methodOverride = require("method-override");
 const passport = require("passport")
 const User = require("./models/User")
+const LocalStrategy = require("passport-local");
+const flash = require("connect-flash")
 
 
 
@@ -24,6 +26,8 @@ mongoose.connect('mongodb://localhost:27017/hotel-app')
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "views"))
 app.use(methodOverride("_method"))
+app.use(express.urlencoded({extended: true}))
+
 
 
 app.use(cors(
@@ -31,6 +35,24 @@ app.use(cors(
         origin: "http://localhost:3000",
     }
 ))
+
+
+const sessionConfig = {
+    secret: "thisthepassword",
+    saveUninitialized: true,
+    resave: false,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+    }
+}
+
+app.use(session(sessionConfig));
+app.use(flash())
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
@@ -43,11 +65,23 @@ app.get("/", (req, res) => {
     res.send("hello imran")
 })
 
-app.post("/login", async (req, res) => {
-    const { email, username, password } = req.body;
+app.post("/login", passport.authenticate("local"), async (req, res) => {
+    try {
+        res.send("post req")
+    }
+    catch {
+        res.send("failed")
+    }
+})
+
+app.post("/register", async (req, res) => {
+    const { username, email, password } = req.body;
     const user = new User({email: email, username: username});
     const newUser = await User.register(user, password);
-    res.send(newUser)
+    
+    
+    console.log(newUser);
+    res.send("adsdas")
 })
 
 app.listen(5000, () => {
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,14 +1,15 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const passportLocalMongoose = require("passport-local-mongoose")
 
 const userSchema = new Schema({
-    name: String,
+    // name: String,
     email: String,
     role: {
         type: String,
         enum: ["owner", "housekeeping", "front office", "supervisor"]
     },
-    password: String,
+    // password: String,
     group: {
         type: Schema.Types.ObjectId,
         ref: "Group"
